da317627ea31355bd751767eb100582fa0dbf090
carlogino <carloginocatapang@gmail.com>
2021-07-14 08:52:00 +0800

    feat(movement): Created movement utility

---
+++ src/utils/__TEST__/movement.spec.ts
@@ -0,0 +1,173 @@
+import { MOVE_DIRECTION } from '../../models/movement-direction.enum';
+import { move } from '../movement';
+import * as faker from 'faker';
+
+test('Move to right from NON right edge of the map', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number({ min: 0, max: DIMENSION - 2 });
+  const y = faker.datatype.number(DIMENSION - 1);
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.RIGHT, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x: x + 1, y });
+});
+
+test('Move to right from right edge of the map', () => {
+  const DIMENSION = 10;
+  const x = DIMENSION - 1;
+  const y = faker.datatype.number(DIMENSION - 1);
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.RIGHT, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x: 0, y });
+});
+
+test('Move left from NON left edge of the map', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number({ min: 1, max: DIMENSION - 1 });
+  const y = faker.datatype.number(DIMENSION - 1);
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.LEFT, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x: x - 1, y });
+});
+
+test('Move left from left edge of the map', () => {
+  const DIMENSION = 10;
+  const x = 0;
+  const y = faker.datatype.number(DIMENSION - 1);
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.LEFT, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x: DIMENSION - 1, y });
+});
+
+test('Move up from NON top edge of the map', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number(DIMENSION - 1);
+  const y = faker.datatype.number({ min: 1, max: DIMENSION - 1 });
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.UP, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x, y: y - 1 });
+});
+
+test('Move up from top edge of the map', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number(DIMENSION - 1);
+  const y = 0;
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.UP, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x, y: DIMENSION - 1 });
+});
+
+test('Move down from NON bottom edge of the map', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number(DIMENSION - 1);
+  const y = faker.datatype.number({ min: 0, max: DIMENSION - 2 });
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.DOWN, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x, y: y + 1 });
+});
+
+test('Move down from bottom edge of the map', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number(DIMENSION - 1);
+  const y = DIMENSION - 1;
+
+  const coordinate = move({ x, y }, MOVE_DIRECTION.DOWN, {
+    height: DIMENSION,
+    width: DIMENSION,
+  });
+
+  expect(coordinate).toEqual({ x, y: 0 });
+});
+
+test('Should throw an exception for invalid direction', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number(DIMENSION - 1);
+  const y = DIMENSION - 1;
+  const INVALID_DIRECTION = faker.random.word() as any;
+
+  expect(() =>
+    move({ x, y }, INVALID_DIRECTION, {
+      height: DIMENSION,
+      width: DIMENSION,
+    }),
+  ).toThrowError('Must have a valid direction');
+});
+
+test('Should throw an exception for negative coordinates', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number(-1);
+  const y = faker.datatype.number(-1);
+
+  expect(() =>
+    move({ x, y }, MOVE_DIRECTION.DOWN, {
+      height: DIMENSION,
+      width: DIMENSION,
+    }),
+  ).toThrowError('Coordinates must only consists of positive value');
+});
+
+test('Should throw an exception if x is greater than or equal to width', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number({ min: DIMENSION });
+  const y = faker.datatype.number(DIMENSION - 1);
+
+  expect(() =>
+    move({ x, y }, MOVE_DIRECTION.DOWN, {
+      height: DIMENSION,
+      width: DIMENSION,
+    }),
+  ).toThrowError(`x coordinate must be less than ${DIMENSION - 1}`);
+});
+
+test('Should throw an exception if y is greater than or equal to height', () => {
+  const DIMENSION = 10;
+  const x = faker.datatype.number(DIMENSION - 1);
+  const y = faker.datatype.number({ min: DIMENSION });
+
+  expect(() =>
+    move({ x, y }, MOVE_DIRECTION.DOWN, {
+      height: DIMENSION,
+      width: DIMENSION,
+    }),
+  ).toThrowError(`y coordinate must be less than ${DIMENSION - 1}`);
+});
+
+test('Should throw an exception if dimension is less than 2', () => {
+  const DIMENSION = faker.datatype.number(1);
+  const x = faker.datatype.number(DIMENSION - 1);
+  const y = faker.datatype.number({ min: DIMENSION });
+
+  expect(() =>
+    move({ x, y }, MOVE_DIRECTION.DOWN, {
+      height: DIMENSION,
+      width: DIMENSION,
+    }),
+  ).toThrowError(`Height and width must be greater than 1`);
+});
---
+++ src/utils/movement.ts
@@ -0,0 +1,48 @@
+import { Coordinate } from '../models/map.model';
+import { MOVE_DIRECTION } from '../models/movement-direction.enum';
+
+export function move(
+  coordinate: Coordinate,
+  direction: MOVE_DIRECTION,
+  dimension: { height: number; width: number }
+): Coordinate {
+  if (dimension.height <= 1 || dimension.width <= 1) {
+    throw new Error('Height and width must be greater than 1');
+  }
+
+  if (coordinate.x < 0 || coordinate.y < 0) {
+    throw new Error('Coordinates must only consists of positive value');
+  }
+
+  if (coordinate.x >= dimension.width) {
+    throw new Error(`x coordinate must be less than ${dimension.width - 1}`);
+  }
+
+  if (coordinate.y >= dimension.height) {
+    throw new Error(`y coordinate must be less than ${dimension.height - 1}`);
+  }
+
+  if (direction === MOVE_DIRECTION.RIGHT) {
+    return {
+      ...coordinate,
+      x: coordinate.x === dimension.width - 1 ? 0 : ++coordinate.x,
+    };
+  } else if (direction === MOVE_DIRECTION.LEFT) {
+    return {
+      ...coordinate,
+      x: coordinate.x === 0 ? dimension.width - 1 : --coordinate.x,
+    };
+  } else if (direction === MOVE_DIRECTION.UP) {
+    return {
+      ...coordinate,
+      y: coordinate.y === 0 ? dimension.height - 1 : --coordinate.y,
+    };
+  } else if (direction === MOVE_DIRECTION.DOWN) {
+    return {
+      ...coordinate,
+      y: coordinate.y === dimension.height - 1 ? 0 : ++coordinate.y,
+    };
+  } else {
+    throw new Error(`Must have a valid direction`);
+  }
+}
